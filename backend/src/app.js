934b39ddf38d2b6efdc596ed6355e31535e09ac0
Yassine El hassani <yassine.ds.404@gmail.com>
2025-11-12 11:45:00 +0100

    Improve Express app configuration and middleware setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+const qs = require('qs');
 const connectDB = require('./config/database');
 const logger = require('./config/logger');
 const routes = require('./routes');
@@ -17,6 +18,20 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware to parse nested x-www-form-urlencoded data using qs
+app.use((req, res, next) => {
+  if (req.is('application/x-www-form-urlencoded') && req.body) {
+    req.body = qs.parse(req.body, { 
+      allowDots: true,
+      depth: 10,
+      parameterLimit: 1000
+    });
+  }
+  next();
+});
+
 app.use(morgan('combined', { stream: logger.stream }));
 
 // Swagger Documentation
